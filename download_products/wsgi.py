fabb044d364bb84aaf4a1b5940cb4972c2cf9b89
Ajul-kushwah <ajulkushwah786@gmail.com>
2021-05-05 22:26:31 +0530

    deal with static files

--- download_products/wsgi.py
+++ download_products/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'download_products.settings')
 
 application = Cling(get_wsgi_application())
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'download_products.settings')
+
